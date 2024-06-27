f45c62f7450d5c2a29be44bd3e18392b2e24ada3
Anas <anassalem.aa@gmail.com>
2024-06-27 23:23:55 +0400

    Add the contact section

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/projects.css";
+import "../styles/components/contact.css";
 import "../styles/utils.css";
