84b447e20d17198420b541d6caf65f0f0263c535
Anas <anassalem.aa@gmail.com>
2024-07-02 20:11:38 +0400

    fix bugs and add meta data

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -5,13 +5,45 @@ const mobileNav = () => {
   const mobileNavLinks = document.querySelectorAll(".mobile-nav__link");
   const toggleTheme = document.querySelectorAll("#theme-toggle");
 
+  const addFocus = () => {
+    mobileNavLinks.forEach((link) => {
+      link.setAttribute("tabindex", "0");
+    });
+
+    toggleTheme.forEach((btn) => {
+      if (
+        btn.classList.contains("mobile-nav__moon") ||
+        btn.classList.contains("mobile-nav__sun")
+      ) {
+        btn.setAttribute("tabindex", "0");
+      }
+    });
+  };
+
+  const removeFocus = () => {
+    mobileNavLinks.forEach((link) => {
+      link.setAttribute("tabindex", "-1");
+    });
+
+    toggleTheme.forEach((btn) => {
+      if (
+        btn.classList.contains("mobile-nav__moon") ||
+        btn.classList.contains("mobile-nav__sun")
+      ) {
+        btn.setAttribute("tabindex", "-1");
+      }
+    });
+  };
+
   const openNav = () => {
+    addFocus();
     openBtn.style.display = "none";
     closeBtn.style.display = "block";
     mobileNav.style.right = "1rem";
   };
 
   const closeNav = () => {
+    removeFocus();
     const isMobile = window.innerWidth < 768;
     openBtn.style.display = isMobile ? "block" : "none";
     closeBtn.style.display = "none";
@@ -30,7 +62,25 @@ const mobileNav = () => {
     }
   };
 
+  const handleFocus = () => {
+    mobileNavLinks.forEach((link) => {
+      link.setAttribute("tabindex", "-1");
+    });
+
+    toggleTheme.forEach((btn) => {
+      if (
+        btn.classList.contains("mobile-nav__moon") ||
+        btn.classList.contains("mobile-nav__sun")
+      ) {
+        btn.setAttribute("tabindex", "-1");
+      }
+    });
+  };
+
+  handleFocus();
+
   openBtn.addEventListener("click", openNav);
+  
   closeBtn.addEventListener("click", closeNav);
 
   mobileNavLinks.forEach((link) => {
@@ -41,9 +91,12 @@ const mobileNav = () => {
     btn.addEventListener("click", closeNav);
   });
 
-  window.addEventListener('scroll', closeNav);
+  window.addEventListener("scroll", closeNav);
 
-  window.addEventListener("resize", handleResize);
+  window.addEventListener("resize", () => {
+    handleResize();
+    handleFocus();
+  });
 };
 
 export default mobileNav;
