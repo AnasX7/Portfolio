5c861f781601faad2f4255a5fdd58f6691c2d03f
Anas <anassalem.aa@gmail.com>
2024-07-23 02:31:43 +0400

    update scrollrevel animtaion & update project card

--- src/utils/scrollreveal-animation.js
+++ src/utils/scrollreveal-animation.js
@@ -3,90 +3,98 @@ import ScrollReveal from "scrollreveal";
 const sr = ScrollReveal();
 
 const scrollrevealAnimation = () => {
-    const scrollRevealOptions = {
-        origin: "bottom",
-        distance: "50px",
-        duration: 1000,
-    }
+  const scrollRevealOptions = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+    easing: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+    viewFactor: 0.25,
+  };
 
-    sr.reveal(".hero__img", {
-        ...scrollRevealOptions,
-    })
+  sr.reveal(".hero__img", {
+    ...scrollRevealOptions,
+    scale: 1.1,
+  });
 
-    sr.reveal(".hero__subtitle-wrapper", {
-        ...scrollRevealOptions,
-        delay: 500,
-    })
+  sr.reveal(".hero__subtitle-wrapper", {
+    ...scrollRevealOptions,
+    delay: 500,
+    scale: 1.1,
+  });
 
-    sr.reveal(".hero__title", {
-        ...scrollRevealOptions,
-        delay: 1000,
-    })
+  sr.reveal(".hero__title", {
+    ...scrollRevealOptions,
+    delay: 1000,
+    scale: 1.1,
+  });
 
-    sr.reveal(".hero__description", {
-        ...scrollRevealOptions,
-        delay: 1500,
-    })
+  sr.reveal(".hero__description", {
+    ...scrollRevealOptions,
+    delay: 1500,
+    scale: 1.1,
+  });
 
-    sr.reveal(".hero__btn-wrapper", {
-        ...scrollRevealOptions,
-        delay: 1800,
-    })
+  sr.reveal(".hero__btn-wrapper", {
+    ...scrollRevealOptions,
+    delay: 1800,
+    scale: 1.1,
+  });
 
-    sr.reveal(".about__title", {
-        ...scrollRevealOptions,
-    })
+  sr.reveal(".about__title", {
+    ...scrollRevealOptions,
+    scale: 1.1,
+  });
 
-    sr.reveal(".about__description", {
-        ...scrollRevealOptions,
-        interval: 500,
-    })
+  sr.reveal(".about__description", {
+    ...scrollRevealOptions,
+    interval: 500,
+    scale: 1.1,
+  });
 
-    sr.reveal(".about__subtitle", {
-        ...scrollRevealOptions,
-        delay: 1000,
-    })
+  sr.reveal(".about__subtitle", {
+    ...scrollRevealOptions,
+    delay: 1200,
+    scale: 1.1,
+  });
 
-    sr.reveal(".about__list", {
-        ...scrollRevealOptions,
-        interval: 350,
-        delay: 1500,
-    })
+  sr.reveal(".about__ul", {
+    ...scrollRevealOptions,
+    delay: 1250,
+    scale: 1.1,
+  });
 
-    sr.reveal(".about__wrapper", {
-        ...scrollRevealOptions,
-        delay: 2500,
-    })
+  sr.reveal(".about__wrapper", {
+    ...scrollRevealOptions,
+    delay: 2000,
+    scale: 1.1,
+  });
 
-    sr.reveal(".projects__title", {
-        ...scrollRevealOptions,
-        interval: 500,
-    })
+  sr.reveal(".projects__title", {
+    ...scrollRevealOptions,
+    scale: 1.1,
+  });
 
-    sr.reveal(".projects__card", {
-        ...scrollRevealOptions,
-        interval: 500,
-    })
+  sr.reveal(".contact", {
+    ...scrollRevealOptions,
+  });
 
-    sr.reveal(".contact__go-home", {
-        ...scrollRevealOptions,
-    })
+  sr.reveal(".contact__go-home", {
+    ...scrollRevealOptions,
+    scale: 1.1,
+  });
 
-    sr.reveal(".contact__title", {
-        ...scrollRevealOptions,
-        delay: 500,
-    })
+  sr.reveal(".contact__title", {
+    ...scrollRevealOptions,
+    delay: 500,
+    scale: 1.1,
+  });
 
-    sr.reveal(".contact__soical a", {
-        ...scrollRevealOptions,
-        delay: 1000,
-        interval: 300,
-    })
-
-    sr.reveal(".contact__wrapper", {
-        ...scrollRevealOptions,
-        delay: 2000,
-    })
+  sr.reveal(".contact__soical a", {
+    ...scrollRevealOptions,
+    delay: 1000,
+    interval: 300,
+    scale: 1.3,
+  });
 };
 
 export default scrollrevealAnimation;
